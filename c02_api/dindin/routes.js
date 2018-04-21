80246fcf0752e52703a9709c415bd597b94dcf77
demangels <764083003@qq.com>
2018-04-21 22:16:56 +0800

    模块化handler

--- c02_api/dindin/routes.js
+++ c02_api/dindin/routes.js
@@ -1,35 +1,12 @@
 'use strict';
 
+const Recipes = require('./handlers/recipes');
 module.exports = [{
   method: 'GET',
   path: '/api/recipes',
-  handler: function (request, reply) {
-    let sql = 'SELECT * FROM recipes';
-    const params = [];
-    if (request.query.cuisine) {
-      sql += ' WHERE cuisine = ?';
-      params.push(request.query.cuisine);
-    }
-    this.db.all(sql, params, (err, results) => {
-      if (err) {
-        throw err;
-      }
-      reply(results);
-    });
-  }
+  handler: Recipes.find
 }, {
   method: 'GET',
   path: '/api/recipes/{id}',
-  handler: function (request, reply) {
-    this.db.get('SELECT * FROM recipes WHERE id = ?', [request.params.id],
-        (err, result) => {
-          if (err) {
-            throw err;
-          }
-          if (typeof result !== 'undefined') {
-            return reply(result);
-          }
-          return reply('Not found').code(404);
-        });
-  }
+  handler: Recipes.findOne
 }];
