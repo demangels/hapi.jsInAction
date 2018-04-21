e844ec29f6c6c9ca2a53dc84900fd8c7c21e0f14
demangels <764083003@qq.com>
2018-04-21 20:18:49 +0800

    获取所有食谱接口

--- c02_api/dindin/index.js
+++ c02_api/dindin/index.js
@@ -1,18 +1,23 @@
+'use strict';
+
+const Hapi = require('hapi');
 const Sqlite3 = require('sqlite3');
 const db = new Sqlite3.Database('./dindin.sqlite');
-db.all('SELECT * FROM recipes', (err, results) => {
-  if (err) {
-    throw err;
+const server = new Hapi.Server();
+
+server.connection({port: 4000});
+server.route([{
+  method: 'GET',
+  path: '/api/recipes',
+  handler: function (request, reply) {
+    db.all('SELECT * FROM recipes', (err, results) => {
+      if (err) {
+        throw err;
+      }
+      reply(results);
+    });
   }
-  for (let i = 0; i < results.length; ++i) {
-    console.log(results[i].name);
-  }
-});
-db.all('SELECT * FROM recipes WHERE cuisine = ?', ['American'], (err, results) => {
-  if (err) {
-    throw err;
-  }
-  for (let i = 0; i < results.length; ++i) {
-    console.log(results[i].name);
-  }
+}]);
+server.start(() => {
+  console.log('Server listening at: ', server.info.uri);
 });
