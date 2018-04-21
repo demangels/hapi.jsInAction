bda7a9fbe832e369f9c800eb4d0dabf688187520
demangels <764083003@qq.com>
2018-04-21 22:05:17 +0800

    模块化路由

--- c02_api/dindin/index.js
+++ c02_api/dindin/index.js
@@ -6,39 +6,8 @@ const db = new Sqlite3.Database('./dindin.sqlite');
 const server = new Hapi.Server();
 
 server.connection({port: 4000});
-server.route([{
-  method: 'GET',
-  path: '/api/recipes',
-  handler: function (request, reply) {
-    let sql = 'SELECT * FROM recipes';
-    const params = [];
-    if (request.query.cuisine) {
-      sql += ' WHERE cuisine = ?';
-      params.push(request.query.cuisine);
-    }
-    db.all(sql, params, (err, results) => {
-      if (err) {
-        throw err;
-      }
-      reply(results);
-    });
-  }
-}, {
-  method: 'GET',
-  path: '/api/recipes/{id}',
-  handler: function (request, reply) {
-    db.get('SELECT * FROM recipes WHERE id = ?', [request.params.id],
-        (err, result) => {
-          if (err) {
-            throw err;
-          }
-          if (typeof result !== 'undefined') {
-            return reply(result);
-          }
-          return reply('Not found').code(404);
-    });
-  }
-}]);
+server.bind({db: db});
+server.route(require('./routes'));
 server.start(() => {
   console.log('Server listening at: ', server.info.uri);
 });
