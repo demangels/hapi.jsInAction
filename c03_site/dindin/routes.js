32876a9153d6d4d9c4b0e6ee836d4126f010fc18
demangels <764083003@qq.com>
2018-04-22 23:22:40 +0800

    文件服务

--- c03_site/dindin/routes.js
+++ c03_site/dindin/routes.js
@@ -1,3 +1,10 @@
 'use strict';
 
-module.exports = [];
+const Pages = require('./handlers/pages');
+const Assets = require('./handlers/assets');
+
+module.exports = [{
+  method: 'GET',
+  path: '/{param*}',
+  handler: Assets.servePublicDirectory
+}];
