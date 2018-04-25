e19aae36c8e73b1332fce79fb9f17f65a5d826d2
demangels <764083003@qq.com>
2018-04-25 11:48:39 +0800

    国际化项目框架

---
+++ c04_route/i18n/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Path = require('path');
+const server = new Hapi.Server();
+
+server.connection({port: 4000});
+server.register([
+  require('vision')
+], (err) => {
+  if (err) {
+    throw err;
+  }
+  server.views({
+    engines: {
+      hbs: require('handlebars')
+    },
+    path: Path.join(__dirname, 'templates'),
+  });
+
+  server.route([{
+    method: 'GET',
+    path: '/',
+    handler: {
+      view: 'index'
+    }
+  }]);
+  server.start(() => {
+    console.log('Started server at', server.info.uri);
+  });
+});
