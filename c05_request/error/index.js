d54cec72b8f6b95050025874d7e1742ef5dbba3a
demangels <764083003@qq.com>
2018-05-04 23:14:41 +0800

    简单json错误响应

---
+++ c05_request/error/index.js
@@ -0,0 +1,16 @@
+const Boom = require('boom');
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+server.connection({port: 4000});
+server.route({
+  method: 'GET',
+  path: '/v4/users',
+  handler: function (request, reply) {
+    const err = Boom.notImplemented('Still working on this');
+    reply(err);
+  }
+});
+server.start(() => {
+  console.log('Server running at:', server.info.uri);
+});
